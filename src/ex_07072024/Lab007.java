44c9a4cf96e6700919d26aaffa013493ecd3faf8
Ruchita Awalkar <ruchitaawalkar@gmail.com>
2024-07-09 18:12:07 -0500

    All files are added

--- src/ex_07072024/Lab007.java
+++ src/ex_07072024/Lab007.java
@@ -4,7 +4,7 @@ public class Lab007 {
     public static void main(String[] args) {
         System.out.println("Hello World");
 
-        System.out.print("Hello World");
+        System.out.print("Hello World1");
         System.out.print("Hello World");
     }
 }
